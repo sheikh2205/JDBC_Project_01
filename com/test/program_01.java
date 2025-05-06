55c392e642bdd9be4c2a2e54703f639f8404eb26
Sheikh Junaid Ulla A <sheikhjunaid2205@gmail.com>
2025-05-06 09:31:53 +0530

    Update program_01.java

--- com/test/program_01.java
+++ com/test/program_01.java
@@ -3,5 +3,9 @@ package com.test;
 public class Program_01{
 	public static void main (String[]args) {
 		System.out.println("Hello World");
+		scanner s=new Scanner(system.in);
+		system.out.print("Enter a number:"):
+			int n =s.nextInt():
+			system.out.println("Square of "+ n +" is: "+(n*n));
 	}
 }
